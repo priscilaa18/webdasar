9ec0d33dcad521723b21c8e8bb4232e0ef619ef7
Priscila18 <jeanypriscila@student.ub.ac.id>
2023-04-10 15:42:32 +0700

    tambahan file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $nama_kamu= "Bobi";
+    $umur = 5;
+    // tanda petik dua
+    /*
+    echo "Hello World, welcome ".$nama_kamu;
+    echo " Umur Kamu ".$umur;
+    echo " .tahun ";
+    echo "<br/>";
+    */
+    echo "Hello World, welcome ".$nama_kamu; " Umur Kamu ".$umur." tahun";
+    echo "</br>"
+    echo "Berarti $nama_kamu lahir di tahun 2023"
+    // tanda petik satu
+    // 'Hello world';
+?>
